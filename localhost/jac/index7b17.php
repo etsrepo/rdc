fb7b2393118c05e30b7a63fff805ede1b2c34089
C P <cp@evatechsoft.com>
2013-12-30 17:02:44 +0530

    asdfg

---
+++ localhost/jac/index7b17.php
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom"  xml:lang="en-gb">
+	<title type="text">Joomla Template for University and school websites - JA University - Ravindra Degree College</title>
+	<subtitle type="text"></subtitle>
+	<link rel="alternate" type="text/html" href="http://localhost"/>
+	<id>http://localhost/jac/index.php</id>
+	<updated>2013-12-30T16:48:46+05:30</updated>
+	<generator uri="http://joomla.org" version="1.6">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="http://localhost/jac/index.php?format=feed&amp;type=atom"/>
+	<entry>
+		<title>Welcome to Ravindra Degree College</title>
+		<link rel="alternate" type="text/html" href="http://localhost/jac/index.php/89-categories/articles/71-welcome-to-ravindra-degree-college"/>
+		<published>2012-08-06T10:12:56+05:30</published>
+		<updated>2012-08-06T10:12:56+05:30</updated>
+		<id>http://localhost/jac/index.php/89-categories/articles/71-welcome-to-ravindra-degree-college</id>
+		<summary type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;&lt;span style=&quot;color: #222222; font-family: arial, sans-serif; font-size: 13px; line-height: normal;&quot;&gt;Ravindra degree college is a part of ravindra educational institutions which was established in the year 1985. it is a pioneer institute imparting the knowledge in the areas of lifesciences and commerce.&lt;/span&gt;&lt;br style=&quot;color: #222222; font-family: arial, sans-serif; font-size: 13px; line-height: normal;&quot; /&gt;&lt;span style=&quot;color: #222222; font-family: arial, sans-serif; font-size: 13px; line-height: normal;&quot;&gt;         ravindra institutions under the dynamic leadership of honarable secretary g.pullaiah garu , madam principal mamatha garu , has been strengthening college developement activities to bring fruits of research for betterment of society with the able support and vauble guidance of a the faculity.&lt;/span&gt;&lt;br style=&quot;color: #222222; font-family: arial, sans-serif; font-size: 13px; line-height: normal;&quot; /&gt;&lt;span style=&quot;color: #222222; font-family: arial, sans-serif; font-size: 13px; line-height: normal;&quot;&gt;           the college offers 5 b ranches in the UG level. the college has excelent infrastructural facilities including modern laboratories, library and also good hostal facility . the college also provides a facility of scholarship to the students.&lt;/span&gt;&lt;/p&gt;&lt;/div&gt;</summary>
+		<content type="html">&lt;div class=&quot;feed-description&quot;&gt;&lt;p&gt;&lt;span style=&quot;color: #222222; font-family: arial, sans-serif; font-size: 13px; line-height: normal;&quot;&gt;Ravindra degree college is a part of ravindra educational institutions which was established in the year 1985. it is a pioneer institute imparting the knowledge in the areas of lifesciences and commerce.&lt;/span&gt;&lt;br style=&quot;color: #222222; font-family: arial, sans-serif; font-size: 13px; line-height: normal;&quot; /&gt;&lt;span style=&quot;color: #222222; font-family: arial, sans-serif; font-size: 13px; line-height: normal;&quot;&gt;         ravindra institutions under the dynamic leadership of honarable secretary g.pullaiah garu , madam principal mamatha garu , has been strengthening college developement activities to bring fruits of research for betterment of society with the able support and vauble guidance of a the faculity.&lt;/span&gt;&lt;br style=&quot;color: #222222; font-family: arial, sans-serif; font-size: 13px; line-height: normal;&quot; /&gt;&lt;span style=&quot;color: #222222; font-family: arial, sans-serif; font-size: 13px; line-height: normal;&quot;&gt;           the college offers 5 b ranches in the UG level. the college has excelent infrastructural facilities including modern laboratories, library and also good hostal facility . the college also provides a facility of scholarship to the students.&lt;/span&gt;&lt;/p&gt;&lt;/div&gt;</content>
+		<category term="Featured" />
+		<category term="Articles" />
+		<category term="Categories" />
+	</entry>
+</feed>
---
+++ localhost/jac/indexc0d0.php
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! - Open Source Content Management" -->
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>Joomla Template for University and school websites - JA University - Ravindra Degree College</title>
+		<description><![CDATA[]]></description>
+		<link>http://localhost/jac/index.php/component/content/featured</link>
+		<lastBuildDate>Mon, 30 Dec 2013 16:48:44 +0530</lastBuildDate>
+		<generator>Joomla! - Open Source Content Management</generator>
+		<atom:link rel="self" type="application/rss+xml" href="http://localhost/jac/index.php?format=feed&amp;type=rss"/>
+		<language>en-gb</language>
+		<item>
+			<title>Welcome to Ravindra Degree College</title>
+			<link>http://localhost/jac/index.php/89-categories/articles/71-welcome-to-ravindra-degree-college</link>
+			<guid isPermaLink="true">http://localhost/jac/index.php/89-categories/articles/71-welcome-to-ravindra-degree-college</guid>
+			<description><![CDATA[<div class="feed-description"><p><span style="color: #222222; font-family: arial, sans-serif; font-size: 13px; line-height: normal;">Ravindra degree college is a part of ravindra educational institutions which was established in the year 1985. it is a pioneer institute imparting the knowledge in the areas of lifesciences and commerce.</span><br style="color: #222222; font-family: arial, sans-serif; font-size: 13px; line-height: normal;" /><span style="color: #222222; font-family: arial, sans-serif; font-size: 13px; line-height: normal;">         ravindra institutions under the dynamic leadership of honarable secretary g.pullaiah garu , madam principal mamatha garu , has been strengthening college developement activities to bring fruits of research for betterment of society with the able support and vauble guidance of a the faculity.</span><br style="color: #222222; font-family: arial, sans-serif; font-size: 13px; line-height: normal;" /><span style="color: #222222; font-family: arial, sans-serif; font-size: 13px; line-height: normal;">           the college offers 5 b ranches in the UG level. the college has excelent infrastructural facilities including modern laboratories, library and also good hostal facility . the college also provides a facility of scholarship to the students.</span></p></div>]]></description>
+			<category>Featured</category>
+			<category>Articles</category>
+			<category>Categories</category>
+			<pubDate>Mon, 06 Aug 2012 10:12:56 +0530</pubDate>
+		</item>
+	</channel>
+</rss>
